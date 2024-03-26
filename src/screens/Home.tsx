54a48c1bf65daf24a76c4cf9bf4e0775aa45dd02
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-03-26 17:14:46 -0300

    added serial number search to home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,35 @@
 import { NavigationProp } from "@react-navigation/native";
-import { Button, StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import Toast from "react-native-toast-message";
 
 const Home = ({ navigation }: { navigation: NavigationProp<any> }) => {
+  const [serialNumber, setSerialNumber] = useState<string | null>(null);
+
+  const handleSubmit = () => {
+    if (serialNumber) {
+      navigation.navigate("ViewPrinter", { serialNumber });
+    } else {
+      Toast.show({
+        type: "error",
+        text1: "Digite um Número de Série",
+        position: "bottom",
+        visibilityTime: 5000,
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Button title="Escanear um produto" onPress={() => navigation.navigate("Scan")} />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite um Número de Série"
+        onChangeText={(text) => setSerialNumber(text)}
+      />
+      <Button title="Buscar" onPress={handleSubmit} />
+      <Text>Ou</Text>
+      <Button title="Escanear QR Code" onPress={() => navigation.navigate("Scan")} />
     </View>
   );
 };
@@ -13,7 +38,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignContent: "center",
     alignItems: "center",
+    rowGap: 15,
+  },
+  input: {
+    padding: 10,
+    backgroundColor: "white",
+    borderColor: "gray",
+    textAlign: "center",
+    borderBottomWidth: 2,
+    color: "gray",
   },
 });
 
