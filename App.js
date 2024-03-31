452fde697fd61668d844e0b89a420f9c567611d0
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-03-31 17:13:34 -0300

    added boottom tab navigation
    - created PrinterStack
    - moved Home screen in to stack
    - separated Scan screen from stack

--- App.js
+++ App.js
@@ -1,47 +1,30 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 
 // components
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Toast from "react-native-toast-message";
-import CheckHistory from "./src/screens/CheckHistory";
-import CheckPrinter from "./src/screens/CheckPrinter";
-import Home from "./src/screens/Home";
+import PrinterStackScreen from "./src/screens/PrinterStack";
 import Scan from "./src/screens/Scan";
-import ViewPrinter from "./src/screens/ViewPrinter";
-import ViewPrinterCheck from "./src/screens/ViewPrinterCheck";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Scan" component={Scan} options={{ title: "Escaneie um QR Code" }} />
-        <Stack.Screen
-          name="ViewPrinter"
-          component={ViewPrinter}
-          options={{ title: "Informações da impressora" }}
-        />
-        <Stack.Screen
-          name="CheckPrinter"
-          component={CheckPrinter}
-          options={{ title: "Conferir impressora" }}
-        />
-        <Stack.Screen
-          name="CheckHistory"
-          component={CheckHistory}
-          options={{ title: "Histórico de conferências" }}
-        />
-        <Stack.Screen
-          name="ViewPrinterCheck"
-          component={ViewPrinterCheck}
-          options={{ title: "Visualizar conferência" }}
-        />
-      </Stack.Navigator>
-      <Toast />
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="PrinterStack"
+            component={PrinterStackScreen}
+            options={{ title: "Home", headerShown: false }}
+          />
+          <Tab.Screen name="Scan" component={Scan} options={{ title: "Escaneie um QR Code" }} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      <Toast bottomOffset={60} />
+    </GestureHandlerRootView>
   );
 };
 
---
+++ src/screens/PrinterStack.tsx
@@ -0,0 +1,57 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect } from "react";
+
+// components
+import { NavigationProp } from "@react-navigation/native";
+import CheckHistory from "./CheckHistory";
+import CheckPrinter from "./CheckPrinter";
+import Home from "./Home";
+import ViewPrinter from "./ViewPrinter";
+import ViewPrinterCheck from "./ViewPrinterCheck";
+
+const PrinterStack = createStackNavigator();
+
+const PrinterStackScreen = ({ navigation }: { navigation: NavigationProp<any> }) => {
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("blur", () => {
+      navigation.reset({ index: 1, routes: [{ name: "Home" }] });
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <PrinterStack.Navigator>
+      <PrinterStack.Screen name="Home" component={Home} options={{ title: "Home" }} />
+      <PrinterStack.Screen
+        // @ts-ignore
+        name="ViewPrinter"
+        // @ts-ignore
+        component={ViewPrinter}
+        options={{ title: "Informações da impressora" }}
+      />
+      <PrinterStack.Screen
+        // @ts-ignore
+        name="CheckPrinter"
+        // @ts-ignore
+        component={CheckPrinter}
+        options={{ title: "Conferir impressora" }}
+      />
+      <PrinterStack.Screen
+        // @ts-ignore
+        name="CheckHistory"
+        // @ts-ignore
+        component={CheckHistory}
+        options={{ title: "Histórico de conferências" }}
+      />
+      <PrinterStack.Screen
+        // @ts-ignore
+        name="ViewPrinterCheck"
+        // @ts-ignore
+        component={ViewPrinterCheck}
+        options={{ title: "Visualizar conferência" }}
+      />
+    </PrinterStack.Navigator>
+  );
+};
+
+export default PrinterStackScreen;
