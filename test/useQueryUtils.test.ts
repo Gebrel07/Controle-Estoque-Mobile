e1fc4aa3caa454660cf8d8d00313a5bc38bd84b5
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-04-02 17:52:23 -0300

    added tests with jest
    - added test for leftJoinQueriesOnKey

---
+++ test/useQueryUtils.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from "@jest/globals";
+import { useQueryUtils } from "../src/hooks/useQueryUtils";
+
+describe("leftJoinQueriesOnKey", () => {
+  const { leftJoinQueriesOnKey } = useQueryUtils();
+
+  test("joins queries and returns single query", () => {
+    const leftQuery = [{ id: 1, a: 123, b: 456 }];
+    const rightQuery = [{ id: 1, a: 321, b: 654, c: 987 }];
+
+    expect(leftJoinQueriesOnKey(leftQuery, rightQuery, "id", "id")).toStrictEqual([
+      { id: 1, a: 321, b: 654, c: 987 },
+    ]);
+  });
+});
