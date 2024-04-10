b70c9e3c3772525db38a18fd31e28dcd6fc9b117
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-04-10 16:20:04 -0300

    added Address type

--- src/types/clientTypes.ts
+++ src/types/clientTypes.ts
@@ -1,11 +1,13 @@
+export interface Address {
+  street: string;
+  number: number;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
 export interface Client {
   id: string;
   name: string;
-  address: {
-    street: string;
-    number: number;
-    city: string;
-    state: string;
-    zipCode: string;
-  };
+  address: Address;
 }
